3f7bc97bf7ff74f85bb7d55175dffacda5aa46a3
CodeLHC <lhcrawford97@gmail.com>
2023-03-25 12:55:17 +0000

    location details component

---
+++ src/components/LocationDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LocationDetails({ city, country }) {
+  return (
+    <div>
+      <h1 className="locationInfo">{`${city}, ${country}`}</h1>
+    </div>
+  );
+}
+
+export default LocationDetails;
