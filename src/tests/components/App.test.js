44a9bd5c6f0bd42a89323f061a63c99bacd2ccf7
CodeLHC <lhcrawford97@gmail.com>
2023-04-01 14:16:26 +0100

    updated app testing

--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -1,7 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import React from "react";
 import App from "../../components/App";
-import forecast from "../../data/forecast.json";
 
 describe("App", () => {
   const location = {
