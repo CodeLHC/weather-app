9db7fd681f2ae4d2c5334c3539ea5e71f0a6a07a
CodeLHC <lhcrawford97@gmail.com>
2023-06-21 16:18:55 +0100

    updates to searchForm styling

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import "../styles/SearchForm.css";
 
 function SearchForm({ searchText, setSearchText, onSubmit }) {
   const handleInputChange = (event) => setSearchText(event.target.value);
   return (
     <div className="search-form">
-      <input type="text" onChange={handleInputChange} value={searchText} />
-      <button type="submit" onClick={onSubmit}>
-        Search
+      <input
+        className="search-form__input"
+        type="text"
+        onChange={handleInputChange}
+        value={searchText}
+        placeholder="Enter UK City for forecast details!"
+      />
+      <button className="search-form__button" type="submit" onClick={onSubmit}>
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          style={{ color: "#2B2D42" }}
+        />
       </button>
     </div>
   );
